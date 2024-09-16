8cb7df4482e26f05c566560748914669d4b45e80
TiagoLDLL <168130334+TiagoLDLL@users.noreply.github.com>
2024-09-16 16:22:13 -0300

    Delete sistema-reservas-hotel directory

--- sistema-reservas-hotel/src/interfaces/ServicoDePagamento.java
+++
@@ -1,8 +0,0 @@
-package interfaces;
-
-public interface ServicoDePagamento {
-
-    String getOpcaoPagamento();
-    double getValor();
-    
-}
--- sistema-reservas-hotel/src/status/StatusQuarto.java
+++
@@ -1,6 +0,0 @@
-package status;
-
-public enum StatusQuarto {
-    RESERVADO,
-    DISPONIVEL
-}
